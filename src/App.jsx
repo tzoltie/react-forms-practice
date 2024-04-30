76bfe0d4838cb6b67b703fb267d74e5676479953
Tim Zoltie <t.zoltie@gmail.com>
2024-04-30 11:05:02 +0100

    Add: Name, number, address etc being added to the state onChange

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,53 @@ export default function App() {
 
   //TODO: Add your state fields here
 
+  const [formState, setFormState] = useState([])
+
+  const [formData, setFormData] = useState({
+    name: '',
+    address: '',
+    phoneNumber: '', 
+    email: ''
+  })
+
+  const handleChange = (event) => {
+    const {name, value} = event.target
+
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+    console.log(formData)
+  }
+
+  const handleSubmit = () => {
+    setFormState({
+      ...formState, 
+      formData
+    })
+  }
+
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h2>Complaining form!</h2>
         <div className="form__section-left">
           <label>
             Full name
-            <input type="text" name="name" required />
+            <input type="text" name="name" value={formData.value} onChange={(event) => handleChange(event)} required />
           </label>
           <label>
             Address
-            <input type="text" name="address" />
+            <input type="text" name="address" value={formData.value} onChange={(event) => handleChange(event)}/>
           </label>
           <label>
             Phone Number
-            <input type="tel" name="phone" />
+            <input type="tel" name="phone" value={formData.value} onChange={(event) => handleChange(event)}/>
           </label>
 
           <label>
             Email
-            <input type="email" name="email" />
+            <input type="email" name="email" value={formData.value} onChange={(event) => handleChange(event)}/>
           </label>
         </div>
 
